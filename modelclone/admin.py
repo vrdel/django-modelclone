d3d97f2245ec82b17f84ba07fe00fd7a5736bc67
Daniel Vrcic <daniel.vrcic@gmail.com>
2018-07-20 11:52:47 +0200

    refined check of Django versions for urlpatterns

--- modelclone/admin.py
+++ modelclone/admin.py
@@ -46,14 +46,14 @@ class ClonableModelAdmin(ModelAdmin):
             self.model._meta.app_label,
             getattr(self.model._meta, 'module_name', getattr(self.model._meta, 'model_name', '')))
 
-        if VERSION[1] < 9:
+        if VERSION[0] == 1 and VERSION[1] < 9:
             from django.conf.urls import patterns
             new_urlpatterns = patterns('',
                 url(r'^(.+)/clone/$',
                     self.admin_site.admin_view(self.clone_view),
                     name=url_name)
                 )
-        else:
+        elif VERSION[0] > 1 or (VERSION[0] == 1 and VERSION[1] >= 9):
             new_urlpatterns = [
                 url(r'^(.+)/change/clone/$',
                     self.admin_site.admin_view(self.clone_view),
